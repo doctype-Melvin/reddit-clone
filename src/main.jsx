fb0d582f4c147e95331a264c84c05a477cc05bad
doctype-Melvin <doctype.melvin@gmail.com>
2023-02-09 07:08:31 +0100

    removes the original useless browser router -- makes sidebar links clickable for full width

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { createBrowserRouter,
-RouterProvider } from 'react-router-dom'
-import Submit from './components/Submit'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "./components/Submit",
-        element: <Submit />
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+   
+    <App />
   </React.StrictMode>,
 )
