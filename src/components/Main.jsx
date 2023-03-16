c1567f84da4dc4e5dfcd6c024c85a2cbdf2ebeda
doctype-Melvin <doctype.melvin@gmail.com>
2023-03-16 06:46:51 +0100

    accidental commit -- changes App.css => app.css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,59 +6,52 @@ import Sidebar from "./Sidebar";
 import Submit from "./Submit";
 
 export default function Main(props) {
-    
-    const [ isPosting, setIsPosting ] = useState(0)
-    
-    
-    // useEffect to rerender feed data
+  const [isPosting, setIsPosting] = useState(0);
 
-    const handlePosting = () => {
-        setIsPosting(prevState => !prevState)
-    }
+  // useEffect to rerender feed data
 
-      return (
-        <div className="mainCt">
-            { isPosting ? (
-            <>
-            <Submit
-            handlePosting={handlePosting} /> 
-            <div className="sidebar">
-                <Sidebar
-                setIsLogin={props.setIsLogin}
-                isLogin={props.isLogin}
-                loggedIn={props.loggedIn}
-                user={props.user}
-                setUser={props.setUser}
-                />
-            </div>
-                </>
-            ) : (
-                <>
-            <div className="createPost">
-                <PostBox
-                handlePosting={handlePosting}
-                />
-            </div>
-            <div className="filterPosts">
-                <FilterBox />
-            </div>
-            <div className="feed">
-                <Post />
-            </div>
-            <div className="sidebar">
-                <Sidebar 
-                setIsLogin={props.setIsLogin}
-                isLogin={props.isLogin}
-                setLoggedIn={props.setLoggedIn}
-                loggedIn={props.loggedIn}
-                user={props.user}
-                setUser={props.setUser}
-                />
-            </div>
-            </>
-            )
-            }
-        </div>
-        
-    )
+  const handlePosting = () => {
+    setIsPosting((prevState) => !prevState);
+  };
+
+  return (
+    <section className="main__container">
+      {isPosting ? (
+        <>
+          <Submit handlePosting={handlePosting} />
+          <div className="sidebar">
+            <Sidebar
+              setIsLogin={props.setIsLogin}
+              isLogin={props.isLogin}
+              loggedIn={props.loggedIn}
+              user={props.user}
+              setUser={props.setUser}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="createPost">
+            <PostBox handlePosting={handlePosting} />
+          </div>
+          <div className="filterPosts">
+            <FilterBox />
+          </div>
+          <div className="feed">
+            <Post />
+          </div>
+          <div className="sidebar">
+            <Sidebar
+              setIsLogin={props.setIsLogin}
+              isLogin={props.isLogin}
+              setLoggedIn={props.setLoggedIn}
+              loggedIn={props.loggedIn}
+              user={props.user}
+              setUser={props.setUser}
+            />
+          </div>
+        </>
+      )}
+    </section>
+  );
 }
