346305b676f95127b30d6691f40d6b662e11199c
doctype-Melvin <doctype.melvin@gmail.com>
2023-03-16 06:50:52 +0100

    changes css file naming to lower case

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Login from './components/Login'
+import { useState } from "react";
+import "./app.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Login from "./components/Login";
 
 function App() {
   // Switch to login page
-    const [ isLogin, setIsLogin ] = useState(false)
-    // Is user logged in
-    const [ loggedIn, setLoggedIn ] = useState(false)
-    // Save user data
-    const [ user, setUser ] = useState('')
+  const [isLogin, setIsLogin] = useState(false);
+  // Is user logged in
+  const [loggedIn, setLoggedIn] = useState(false);
+  // Save user data
+  const [user, setUser] = useState("");
 
   return (
     <div className="App">
-     <Header />
-     {/* Conditionally render 
+      <Header />
+      {/* Conditionally render 
      the main section depending on if user 
      is logged in or not */}
-     {
-     isLogin ? 
-     <Login
-     setIsLogin={setIsLogin}
-     setLoggedIn={setLoggedIn}
-     setUser={setUser}
-     /> : 
-     <Main
-     setIsLogin={setIsLogin}
-     isLogin={isLogin}
-     setLoggedIn={setLoggedIn}
-     loggedIn={loggedIn}
-     user={user}
-     setUser={setUser}
-      />
-      }
+      {isLogin ? (
+        <Login
+          setIsLogin={setIsLogin}
+          setLoggedIn={setLoggedIn}
+          setUser={setUser}
+        />
+      ) : (
+        <Main
+          setIsLogin={setIsLogin}
+          isLogin={isLogin}
+          setLoggedIn={setLoggedIn}
+          loggedIn={loggedIn}
+          user={user}
+          setUser={setUser}
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
